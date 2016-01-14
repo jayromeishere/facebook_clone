9c33a699e25e759a33ab8b14dd26a8417a9dd3e7
Jerome Sardoma <Jerome@Jeromes-MacBook-Pro.local>
2016-01-14 13:39:56 -0800

    added user login spec

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
   #always include
   #also make sure to end file name with _spec
+  #used with Capybara gem 
 
 feature "User Login" do
-  scenario "successful" do
+  background do
     user = create(:user)
-    
+  end 
+  
+  scenario "successful" do    
     visit root_path
     click_link 'Sign In'
     fill_in 'Email', with: 'email_1@foobar.com'
@@ -14,4 +17,12 @@ feature "User Login" do
     expect(current_path).to eql feed_path
   end
   
+  scenario "unsuccessful" do
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: 'email_1@foobar.com'
+    fill_in 'Password', with: 'bad_password'
+    click_button 'Log in'
+    expect(current_path).to eql new_user_session_path
+  end
 end
